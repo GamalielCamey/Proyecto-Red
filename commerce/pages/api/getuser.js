a0f3676d3962acdc3627d39ee8622e5491aced9c
Gama <lic.camey@gmail.com>
2022-11-16 16:53:51 -0600

    athentication with credentials & conection to DB/ session validation on home page

--- commerce/pages/api/getuser.js
+++ commerce/pages/api/getuser.js
@@ -3,6 +3,11 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  const result = await prisma.user.findMany();
+  const {email} = req.body;
+  const result = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
   res.json(result);
 }
--- commerce/pages/index.js
+++ commerce/pages/index.js
@@ -1,11 +1,27 @@
 import Link from "next/link";
+import {signOut, getSession} from "next-auth/react";
 
-export default function Home() {
+export default function Home({session}) {
   return (
     <div>
-      <Link href="/signin">SignIn</Link>
-
       <h1>Hello World</h1>
+      {session ? <h1>{`Hello ${session.user.name}`}</h1> : null}
+      {session ? (
+        <button onClick={() => signOut()}>SignOut</button>
+      ) : (
+        <Link href="/signin">
+          <button>SignIn</button>
+        </Link>
+      )}
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  return {
+    props: {
+      session: session,
+    },
+  };
+};
