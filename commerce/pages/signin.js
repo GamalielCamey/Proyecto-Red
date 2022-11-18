08e300dc926de1207f56c23f57a02433f46ea6b1
Gama <lic.camey@gmail.com>
2022-11-17 20:43:58 -0600

    Header with style(sketch)

--- commerce/pages/signin.js
+++ commerce/pages/signin.js
@@ -1,22 +1,38 @@
 import {signIn, getProviders} from "next-auth/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGoogle} from "@fortawesome/free-brands-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faDiscord} from "@fortawesome/free-brands-svg-icons";
 
 export default function Signin({providers}) {
   return (
     <div className="signin">
       <h2>Login</h2>
-      {Object.values(providers).map((provider) => (
-        <div className="signin__form" key={provider.name}>
-          <button
-            onClick={() =>
-              signIn(provider.id, {
-                callbackUrl: `${window.location.origin}`,
-              })
-            }
-          >
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+      {Object.values(providers).map((provider) => {
+        return (
+          <div className="signin__form" key={provider.name}>
+            <button
+              className="signin__form-BTN"
+              onClick={() =>
+                signIn(provider.id, {
+                  callbackUrl: `${window.location.origin}`,
+                })
+              }
+            >
+              Sign in with {provider.name}
+              <FontAwesomeIcon
+                icon={
+                  provider.name === "Google"
+                    ? faGoogle
+                    : provider.name === "GitHub"
+                    ? faGithub
+                    : faDiscord
+                }
+              />
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
