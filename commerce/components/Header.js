a980a43b1b5539797e5a168a67ed7824ca08557f
Gama <lic.camey@gmail.com>
2022-11-18 23:36:27 -0600

    Header new style

--- commerce/components/Header.js
+++ commerce/components/Header.js
@@ -1,28 +1,74 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCartShopping, faUser} from "@fortawesome/free-solid-svg-icons";
+import {useState, forwardRef} from "react";
 import Link from "next/link";
 import {signOut} from "next-auth/react";
 
-export default function Header({session}) {
-  console.log(session);
+const Dropdown = forwardRef((props, ref) => {
   return (
-    <div className="container">
-      {session ? (
-        <div className="container__user">
-          <h3>{`Hello ${session.user.name}`}</h3>
-          <img
-            style={{borderRadius: "50%", width: "30px"}}
-            src={session.user.image}
-            alt="user-img"
-          />
+    <>
+      {props.session ? (
+        <div className="dropdown">
+          <Link className="dropdown__item" href="#" onClick={() => signOut()}>
+            Signout
+          </Link>
         </div>
-      ) : null}
-
-      {session ? (
-        <button onClick={() => signOut()}>SignOut</button>
       ) : (
-        <Link href="/signin">
-          <button>SignIn</button>
-        </Link>
+        <div className="dropdown">
+          <Link className="dropdown__item" href="/signin">
+            Signin
+          </Link>
+        </div>
       )}
+    </>
+  );
+});
+
+export default function Header({session}) {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="navbar">
+      <ul className="navbar__nav">
+        <li className="navbar__nav__item">
+          <Link
+            href="#"
+            onClick={() => toggle()}
+            className="navbar__nav__item__button"
+          >
+            {session ? (
+              <img
+                className="navbar__nav__item__button-icon"
+                style={{borderRadius: "50%", width: "30px"}}
+                src={session.user.image}
+                alt="user-img"
+              />
+            ) : (
+              <FontAwesomeIcon
+                className="navbar__nav__item__button-icon"
+                icon={faUser}
+                size="1x"
+              />
+            )}
+
+            {open && <Dropdown session={session} />}
+          </Link>
+        </li>
+
+        <li className="navbar__nav__item">
+          <Link href="#" className="navbar__nav__item__button">
+            <FontAwesomeIcon
+              className="navbar__nav__item__button-icon"
+              icon={faCartShopping}
+              size="1x"
+            />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
